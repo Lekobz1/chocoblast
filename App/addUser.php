e849ebb4f7ba0c7859c98c8291df2fcbcb76dbd9
Lekobz1 <jonathan.simon.pro1@gmail.com>
2023-03-16 15:55:08 +0100

    modif + css

--- App/addUser.php
+++ App/addUser.php
@@ -1,3 +1,12 @@
 <?php
+    //connecter la bdd
+    include '../App/utils/connectBdd/connectBdd.php';
+
+    if (isset($_POST['submit'])){
+
+    }else{
+        echo "merci de remplir le formulaire";
+    }
+
 
 ?>
--- App/vue/view_add_user.php
+++ App/vue/view_add_user.php
@@ -1,10 +1,11 @@
 <!-- partie affichage HTML -->
+<!-- //http://localhost/chocoblast/App/vue/view_add_user.php// chemin d'acces affichage -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../Public/asset/style/main.css">
+    <link rel="stylesheet" href="../../public/asset/style/main.css">
     <script src="../../Public/asset/script/script.js" defer></script>
     <title>Inscription</title>
 </head>
@@ -12,22 +13,22 @@
     <div class="formulaire">
         <h1>Ajouter un utilisateur</h1>
         <form method="POST" action="../addUser.php" enctype="multipart/form-data">
-            <label for="nom_utilisateur">Nom :</label>
-            <input type="text" name="nom_utilisateur" id="nom_utilisateur"><br>
+            <label class="label" for="nom_utilisateur">Nom :</label>
+            <input class="input" type="text" name="nom_utilisateur" id="nom_utilisateur"><br>
 
-            <label for="prenom_utilisateur">Prénom :</label>
-            <input type="text" name="prenom_utilisateur" id="prenom_utilisateur"><br>
+            <label class="label" for="prenom_utilisateur">Prénom :</label>
+            <input class="input" type="text" name="prenom_utilisateur" id="prenom_utilisateur"><br>
 
-            <label for="mail_utilisateur">E-mail :</label>
-            <input type="email" name="mail_utilisateur" id="mail_utilisateur"><br>
+            <label class="label" for="mail_utilisateur">E-mail :</label>
+            <input class="input" type="email" name="mail_utilisateur" id="mail_utilisateur"><br>
 
-            <label for="password_utilisateur">Mot de passe :</label>
-            <input type="password" name="password_utilisateur" id="password_utilisateur"><br>
+            <label class="label" for="password_utilisateur">Mot de passe :</label>
+            <input class="input" type="password" name="password_utilisateur" id="password_utilisateur"><br>
 
-            <label for="image_utilisateur">Image :</label>
-            <input type="file" name="image_utilisateur" id="image_utilisateur"><br>
+            <label class="label" for="image_utilisateur">Image :</label>
+            <input class="input" type="file" name="image_utilisateur" id="image_utilisateur"><br>
 
-            <input type="submit" value="Ajouter">
+            <input class="envoyer" type="submit" value="Ajouter">
         </form>
 
     </div>

26bffd8977276f6dfa07d06c12b48b79138e45cb
Lekobz1 <jonathan.simon.pro1@gmail.com>
2023-03-16 15:03:37 +0100

    mise a jour forme du dossier

--- App/vue/view_add_user.php
+++ App/vue/view_add_user.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
+<!-- partie affichage HTML -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/asset/style/main.css">
-    <script src="../../"
-    <title>Document</title>
+    <link rel="stylesheet" href="../../Public/asset/style/main.css">
+    <script src="../../Public/asset/script/script.js" defer></script>
+    <title>Inscription</title>
 </head>
 <body>
     

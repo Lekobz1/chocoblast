435c45e0a21a92dcf15d6597f901381d488cdc21
Lekobz1 <jonathan.simon.pro1@gmail.com>
2023-03-30 14:10:27 +0200

    creation html et css pour la page home

---
+++ App/vue/View_home.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/asset/style/main.css">
+    <script src="../../Public/asset/script/script.js" defer></script>
+    <title>Home</title>
+</head>
+<body>
+
+<div class="home">
+  <div class="chocoweek"></div>
+  <div class="cardchocoweek"></div>
+  <div class="topsoumis"></div>
+  <div class="topblaster"></div>
+  <div class="cardsoumis"></div>
+  <div class="cardblaster"></div>
+  <div class="topchocoblast"></div>
+  <div class="toprat"></div>
+  <div class="cardchocoblast"></div>
+  <div class="cardrat"></div>
+</div>
+   
+</body>
+</html>

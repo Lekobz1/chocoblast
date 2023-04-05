1a1e9a556fb541db3b60d6caedbd752f9fb0c568
Lekobz1 <jonathan.simon.pro1@gmail.com>
2023-04-05 15:23:13 +0200

    crea fichier configuration

--- App/controler/ControlerSign.php
+++ App/controler/ControlerSign.php
@@ -1,6 +1,16 @@
 <?php 
 $message = "";
 
+//tester si c'est submit
+if (isset($_POST['submit'])){
+    //verifier si le mail et le password sont rempli
+    if (!empty($_POST['mail_utilisateur']) and !empty($_POST['password_utilisateur'])){
+
+    }
+
+
+}
+
 
 include './App/vue/view_sign.php'
 ?>
